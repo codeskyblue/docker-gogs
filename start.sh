bd960e19add13a67ffc1bf4f855665c327f3e1d8
codeskyblue <codeskyblue@gmail.com>
2014-07-08 08:29:46 +0800

    add more directories

--- start.sh
+++ start.sh
@@ -12,7 +12,10 @@ mkdir -p /var/run/sshd
 mv /var/lib/mysql /var/lib/mysql-tmp
 ln -s /srv/gogs/data/mysql /var/lib/mysql
 
-ln -s /src/gogs/data/git /home/git
+ln -s /src/gogs/data/repositories /home/git/gogs-repositories
+rm -R /home/git/.ssh && ln -s /srv/gogs/data/ssh /home/git/.ssh && chown -R git:git /srv/gogs/data/ssh && chmod -R 0700 /srv/gogs/data/ssh && chmod 0700 /home/git/.ssh
+chown -R git:git /srv/gogs/data/repositories
+rm -fr /home/git/gogs/conf && ln -s /srv/gogs/data/conf /home/git/gogs/conf && chown -R git:git /srv/gogs/data/conf
 
 # start mysql
 mysqld_safe &
